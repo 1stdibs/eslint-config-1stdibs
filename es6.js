bdf78e41be1a5415ee053f75fd16bbee436c6f29
John Rodriguez <john@John-Rodriguezs-MacBook-Pro.local>
2017-09-06 12:35:24 -0400

    Add rules for flow type linting

--- es6.js
+++ es6.js
@@ -2,8 +2,7 @@
 
 module.exports = {
     "plugins": [
-        "flowtype",
-        "flow-vars"
+        "flowtype"
     ],
     "parser": "babel-eslint",
     "parserOptions": {
@@ -14,8 +13,15 @@ module.exports = {
     },
     "rules": {
         "strict": 0,
-        "flow-vars/define-flow-type": 1,
-        "flow-vars/use-flow-type": 1,
+        "flowtype/define-flow-type": 1,
+        "flowtype/use-flow-type": 1,
+        "flowtype/boolean-style": [2, "boolean"],
+        "flowtype/no-dupe-keys": 2,
+        "flowtype/no-primitive-constructor-types": 2,
+        "flowtype/no-types-missing-file-annotation": 2,
+        "flowtype/require-valid-file-annotation": [2, "never"],
+        "flowtype/space-after-type-colon": [2, "always"],
+        "flowtype/union-intersection-spacing": [2, "always"],
         "constructor-super": 2,
         "no-const-assign": 2,
         "no-dupe-class-members": 2,
