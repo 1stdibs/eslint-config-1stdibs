774ec298782d08d3ccaa41d823c5dcc4dd53242e
dale tan <dale@1stdibs.com>
2019-05-30 13:18:47 -0400

    changed rules from `off` to `warn`

--- packages/eslint-config-1stdibs/rules/typescript.js
+++ packages/eslint-config-1stdibs/rules/typescript.js
@@ -2,11 +2,12 @@ module.exports = {
     plugins: ['@typescript-eslint/eslint-plugin'],
     rules: {
         // overwrite plugin rules
-        '@typescript-eslint/explicit-member-accessibility': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/explicit-member-accessibility': 'warn',
+        '@typescript-eslint/no-explicit-any': 'warn',
     },
     overrides: [
         {
+            // ignore this spec files
             files: ['**/*+(_|.)spec.+(js|jsx|ts|tsx)'],
             rules: {
                 '@typescript-eslint/explicit-function-return-type': 'off',
