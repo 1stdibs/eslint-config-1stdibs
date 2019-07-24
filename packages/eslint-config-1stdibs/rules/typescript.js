6b000289be68bf6d3462ec4670f813ab3d1dc6a2
dale tan <dale@1stdibs.com>
2019-07-24 10:27:29 -0400

    updated typescript rules

--- packages/eslint-config-1stdibs/rules/typescript.js
+++ packages/eslint-config-1stdibs/rules/typescript.js
@@ -1,17 +1,24 @@
 module.exports = {
     plugins: ['@typescript-eslint/eslint-plugin'],
     rules: {
+        'react/prop-types': 'off',
         // overwrite plugin rules
+        camelcase: 'off',
+        '@typescript-eslint/camelcase': [
+            'error',
+            {
+                ignoreDestructuring: true,
+            },
+        ],
         '@typescript-eslint/explicit-member-accessibility': 'warn',
         '@typescript-eslint/no-explicit-any': 'warn',
-    },
-    overrides: [
-        {
-            // ignore spec files
-            files: ['**/*+(_|.)spec.+(js|jsx|ts|tsx)'],
-            rules: {
-                '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/prefer-interface': 'off',
+        '@typescript-eslint/explicit-function-return-type': [
+            'error',
+            {
+                allowExpressions: true,
+                allowTypedFunctionExpressions: true,
             },
-        },
-    ],
+        ],
+    },
 };
