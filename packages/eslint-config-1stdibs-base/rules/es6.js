41af0cd1d8164f811c3e7c55de1f33bf3a0454bd
dale tan <dale@1stdibs.com>
2019-11-25 11:57:54 -0500

    BREAKING CHANGE: remove `babel-eslint`
    `babel-eslint` only needs to be in the main config for flow

--- packages/eslint-config-1stdibs-base/rules/es6.js
+++ packages/eslint-config-1stdibs-base/rules/es6.js
@@ -1,7 +1,10 @@
 module.exports = {
-    parser: 'babel-eslint',
     parserOptions: {
+        ecmaVersion: 2020,
         sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
     },
     env: {
         es6: true,
