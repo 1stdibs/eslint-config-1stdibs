355432108b895c13a2cc383cb5297e3c3f78b509
dale tan <dale@1stdibs.com>
2020-07-30 16:04:56 -0400

    fixup! bump base config to use prettier@^2.0.0

--- packages/eslint-config-1stdibs-base/rules/style.js
+++ packages/eslint-config-1stdibs-base/rules/style.js
@@ -16,6 +16,5 @@ module.exports = {
         'no-underscore-dangle': 'off',
         'one-var': ['warn', 'never'],
         quotes: 'off',
-        'arrow-parens': 'avoid', // 2.0.0 default = always
     },
 };
