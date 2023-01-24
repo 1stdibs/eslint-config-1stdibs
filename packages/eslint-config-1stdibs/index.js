e2710f3149b5df47569bb06e1de5f2dfde73f1c0
vytautassugintas <vytautas.sugintas@1stdibs.com>
2023-01-24 14:38:45 +0200

    chore: remove flow rules

--- packages/eslint-config-1stdibs/index.js
+++ packages/eslint-config-1stdibs/index.js
@@ -1,9 +1,3 @@
 module.exports = {
     extends: ['eslint-config-1stdibs-base', './rules/react'].map(require.resolve),
-    overrides: [
-        {
-            files: ['*.{js,jsx}'],
-            extends: ['./rules/flowtype'].map(require.resolve),
-        },
-    ],
 };
